e11c8c10803f13d2e9517877d969b26b03f7b276
Sangam Mishra <sangamhindyana@gmail.com>
2022-04-24 23:06:05 +0530

    Added solution - LeetHub

--- Product of Primes - GFG/product-of-primes.cpp
+++ Product of Primes - GFG/product-of-primes.cpp
@@ -10,40 +10,46 @@ using namespace std;
 class Solution{
 public:
 
-  bool    isprime(long long num)
+  bool    isprime(long long number)
             {
-                 if(num<2)
-                    return false;
-                    
-                 if(num == 2)
-                    return true;
-                 
-                 if(num % 2==0 )
-                     return false; 
-                 
-                 
-                
-                    
-                int sqr = (int)sqrt(num);
-                
-                 for(int i =3 ; i<= sqr ; i+= 2)
-                   {
-                       if(num%i==0)
-                           return false;
-                   }
-                return true;
+                  
+    if (number < 2)
+        return false;
+    
+    if (number == 2 || number == 3)
+        return true;    
+    
+    if(number % 2 == 0 || number %3 ==0)
+        return false;
+    
+    int sqr = (int) sqrt(number);
+    int dx = 4;
+    
+    for (int i = 5; i <= sqr; i+=dx) {
+        if (number % i == 0)
+            {return false;
             }
+        dx = - (dx - 6);
+        
+    }
+    
+    return true;
+}
+            
     long long primeProduct(long long L, long long R){
         
         
          long long int mod = 1e9+7;
-         long long    pro= 1;
+         long long  pro= 1;
           for(int i = L; i<=R;i++)
               {
                   if(isprime(i))
                     {pro = ((pro%mod) * i)%mod;
+                     
                     }
-    }return pro;
+    }
+ 
+     return pro;
     }
 };
 
