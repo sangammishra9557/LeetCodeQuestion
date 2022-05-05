1b68382fe8922df35c88db5c74a7e8900cfb45bc
Sangam Mishra <sangamhindyana@gmail.com>
2022-05-06 03:50:08 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (88.07%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,55 +1,68 @@
 class MyStack {
 private:
-    queue<int> q1;
-    queue<int> q2;
+    // queue<int> q1;
+    //  queue <int> q2;
+      queue<int> q;
 public:
     MyStack() {
         
     }
     
     void push(int x) {
-        q1.push(x);
+      //  q1.push(x);
+          q.push(x);
+         for(int i =0 ; i<q.size()-1 ; i++)
+         {
+              q.push(q.front());
+              q.pop();
+         }
+        
+           
         
     }
     
     int pop() {
-         int n = q1.size();
-          for(int i =0 ; i< n-1;i++)
-          {
-              q2.push(q1.front());
-              q1.pop();
-          }
-           int node  = q1.front();
-           q1.pop();
-           while(!q2.empty()){
-               q1.push(q2.front());
-               q2.pop();
-           }
-          return  node;
+//          int n = q1.size();
+//           for(int i =0 ; i< n-1;i++)
+//           {
+//               q2.push(q1.front());
+//               q1.pop();
+//           }
+//            int node  = q1.front();
+//            q1.pop();
+//            while(!q2.empty()){
+//                q1.push(q2.front());
+//                q2.pop();
+//            }
+//           return  node;
+        int node = q.front();
+          q.pop();
+            return node;
         
     }
     
     int top() {
-         int n = q1.size();
-          for(int i =0 ; i< n-1;i++)
-          {
-              q2.push(q1.front());
-              q1.pop();
-          }
-           int node  = q1.front();
-            q1.pop();
+//          int n = q1.size();
+//           for(int i =0 ; i< n-1;i++)
+//           {
+//               q2.push(q1.front());
+//               q1.pop();
+//           }
+//            int node  = q1.front();
+//             q1.pop();
          
-           while(!q2.empty()){
-               q1.push(q2.front());
-               q2.pop();
-           }
-           q1.push(node);
-          return node;
-        
+//            while(!q2.empty()){
+//                q1.push(q2.front());
+//                q2.pop();
+//            }
+//            q1.push(node);
+//           return node;
+            int node = q.front();
+            return node;
     }
     
     bool empty() {
-         return q1.empty();
+         return q.empty();
     }
 };
 
