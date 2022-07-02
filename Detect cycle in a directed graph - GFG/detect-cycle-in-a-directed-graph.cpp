6a7decf4d79d1f5341936e0f8c21e6460a1235fd
Sangam Mishra <sangamhindyana@gmail.com>
2022-07-02 06:28:40 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -7,72 +7,69 @@ class Solution {
   public:
   
    // below is the solution for the kahn's algorithm 
-   bool isCyclic(int V, vector<int> adj[]) {
-       vector<int> indegree(V , 0);
-       vector<int> visited(V , 0);
-       for(int i =0 ; i < V ; i++){
-             for(auto neigh :  adj[i]){
-                indegree[neigh]++;   
-             }
-       }
+//   bool isCyclic(int V, vector<int> adj[]) {
+//       vector<int> indegree(V , 0);
+//       vector<int> visited(V , 0);
+//       for(int i =0 ; i < V ; i++){
+//              for(auto neigh :  adj[i]){
+//                 indegree[neigh]++;   
+//              }
+//       }
        
-        queue<int> q ; 
-        for(int i =0 ; i < V ; i++){
-             if(indegree[i] == 0) q.push(i);
-        }
-        int flag = 0 ; 
-         while(!q.empty()){
-              auto node = q.front();
-              q.pop();
-              if( visited[node]) continue ; 
-              visited[node] = true ;
-              flag++;
-               for(auto child:  adj[node]){
-                   indegree[child] -- ; 
-                   if(indegree[child] == 0)
-                       q.push(child);
-               }
-         }
-          return !(flag == V);
-   }
+//         queue<int> q ; 
+//         for(int i =0 ; i < V ; i++){
+//              if(indegree[i] == 0) q.push(i);
+//         }
+//         int flag = 0 ; 
+//          while(!q.empty()){
+//               auto node = q.front();
+//               q.pop();
+//               if( visited[node]) continue ; 
+//               visited[node] = true ;
+//               flag++;
+//               for(auto child:  adj[node]){
+//                   if(--indegree[child]  == 0)  q.push(child);
+//               }
+//          }
+//           return !(flag == V);
+//   }
   
     // Function to detect cycle in a directed graph.
-    // bool isCyclic(int V, vector<int> adj[]) {
-    // //   vector<int>  visited(V , 0 );
-    // //   vector<int>  rstack(V , 0 );
-    //   int visited[V];
-    //   int rstack[V];
-    //   memset(visited , 0 , sizeof(visited));
-    //   memset(rstack , 0 , sizeof(rstack));
+    bool isCyclic(int V, vector<int> adj[]) {
+    //   vector<int>  visited(V , 0 );
+    //   vector<int>  rstack(V , 0 );
+      int visited[V];
+      int rstack[V];
+      memset(visited , 0 , sizeof(visited));
+      memset(rstack , 0 , sizeof(rstack));
        
-    //     for(int  i =0 ; i< V ; i++)
-    //       {
-    //           if(!visited[i])
-    //             {
-    //                  if(dfscycle( i , adj , visited , rstack))
-    //                   return true ;
-    //             }
+        for(int  i =0 ; i< V ; i++)
+          {
+              if(!visited[i])
+                {
+                     if(dfscycle( i , adj , visited , rstack))
+                      return true ;
+                }
                 
-    //       }
-    //       return false;
-    // }
-    //  bool dfscycle(int v ,  vector<int> adj[] ,int visited [] , int rstack[]){
+          }
+          return false;
+    }
+     bool dfscycle(int v ,  vector<int> adj[] ,int visited [] , int rstack[]){
          
-    //       visited[v] = 1 ; 
-    //       rstack [v] = 1 ; 
+          visited[v] = 1 ; 
+          rstack [v] = 1 ; 
          
-    //       for( auto neigh  : adj[v]){
-    //         if(!visited[neigh] )
-    //           {
-    //               if (dfscycle( neigh , adj , visited ,rstack))
-    //                  return true ; 
-    //           }
+          for( auto neigh  : adj[v]){
+            if(!visited[neigh]   && dfscycle( neigh , adj , visited ,rstack) )
+              {
+                 return true ;
+              }
               
-    //          else if (rstack[neigh])  return true ;
-    //       }
-    //       rstack[v] = 0 ;
-    //       return false;
-    //  }
+             else if (rstack[neigh])  return true ;
+          }
+          rstack[v] = 0 ;
+          return false;
+     }
     
 };
 
