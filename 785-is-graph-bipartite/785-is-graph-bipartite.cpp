0350efc4ac060e0768c3aa60684b9ec5a0c9a04e
Sangam Mishra <sangamhindyana@gmail.com>
2022-07-10 15:38:26 +0530

    Time: 47 ms (32.46%), Space: 15 MB (14.80%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n =  graph.size();
-        vector<int> color( n , -1);
-         for(int i =0 ; i<n ; i++){
-              {   if(color[i] == -1)
-                     if(!hasEvenCycle( i , 0 , graph , color))
-                         return false;
+        
+         int n = graph.size();
+         vector<int>   color(n , -1);
+         for(int i =0 ; i< n ; i++){
+              
+              if(color[i] == -1){
+                   if(!bfsevencycle(i , graph , color))
+                       return false;
               }
          }
          return true;
     }
-    
-     bool  hasEvenCycle(int vertex , int currentColor , vector<vector<int>> &graph ,  vector<int> &color){
-        if(color[vertex]   !=  -1)
-        {
-             return color[vertex] == currentColor;
-        }
-          color[vertex] = currentColor;
-         for(int child : graph[vertex]){
-              
-              if(!hasEvenCycle( child , 1-currentColor ,  graph , color))
-                   return false;
-         }
+    bool bfsevencycle(int vertex , vector<vector<int>> & graph , vector<int> &color){
+         int n = graph.size();
+         queue<pair<int,int>> q ; 
+         q.push({vertex , 0});
+              while(!q.empty()){
+                  auto pr = q.front();
+                   q.pop();
+                  int node = pr.first;
+                  int cc = pr.second;
+                  if(color[node] != -1){
+                      if(color[node] != cc)
+                          return false;
+                  }
+                  else{
+                      color[node] = cc;
+                      for(int  neigh : graph[node]){
+                           
+                          q.push({neigh , 1-cc});
+                          
+                      }
+                  }
+                  
+                  
+                  
+                  cout<<endl;
+              }
          return true;
-      
-     }
+         }
 };
