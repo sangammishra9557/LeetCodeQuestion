faca4541f159bdb01ab3dcf600ee745b930721eb
Sangam Mishra <sangamhindyana@gmail.com>
2022-07-26 17:58:17 +0530

    Time: 145 ms (45.22%), Space: 46.1 MB (17.53%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,22 +1,28 @@
 class Solution {
-private:
-    int dp[21][10001];
-    int coinChangeHelper(vector<int>& coins, int n, int amount)
-    {
-        if(amount == 0) return 0;
-        if(n == 0) return INT_MAX - 1;
-        if(dp[n][amount] != -1) return dp[n][amount];
-        if(amount - coins[n-1] >= 0)
-        {
-            return dp[n][amount] = min( 1 + coinChangeHelper(coins, n, amount - coins[n-1]), coinChangeHelper(coins, n - 1, amount));
-        }
-        return dp[n][amount] = coinChangeHelper(coins, n - 1, amount);
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        memset(dp, -1, sizeof(dp));
-        int result = coinChangeHelper(coins, coins.size(), amount);
-        if(result == INT_MAX || result == INT_MAX - 1) return -1;
-        return result;
+        vector<vector<int>> memo( coins.size() , vector<int> (amount+1 ,  -1));
+      if (mincoin(0 , amount , coins , memo) == 100000000) return -1;
+        return mincoin( 0  , amount , coins , memo);
+        
     }
+
+    int   mincoin(int currentindex , int amount , vector<int> & coins , vector<vector<int>> & memo  ){
+        
+        
+           if(amount == 0 )
+                 return  0 ;
+           if(currentindex >= coins.size())  return  100000000;
+            
+           if(memo[currentindex][amount] != -1) return memo[currentindex][amount];
+        
+           int notpick = mincoin(currentindex+1 , amount , coins ,memo);
+           int pick =   100000000;
+           if(amount >= coins[currentindex])
+               pick =  1+ mincoin(currentindex , amount - coins[currentindex] , coins ,  memo);
+           
+            return memo[currentindex][amount] =   min(pick , notpick);
+    }
+    
+    
 };
