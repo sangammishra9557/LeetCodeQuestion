2d86aa12bc86e9c1e0242c0b745672f7d534ba20
Sangam Mishra <sangamhindyana@gmail.com>
2022-07-26 16:55:15 +0530

    Time: 90 ms (16.69%), Space: 18.8 MB (18.48%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-         unordered_map<string ,int> mp;
+         vector<vector<int>> mp(coins.size() , vector<int> (amount+1 , -1));
          return totalways( 0 , amount , coins , mp);
     }
     
     
     
-     int totalways(int  ci ,  int amount , vector<int> & coins , unordered_map<string ,int> &mp){
+     int totalways(int  ci ,  int amount , vector<int> & coins , vector<vector<int>> &mp){
           if( amount == 0)  return 1 ; 
           if(ci >= coins.size())  return  0 ; 
          
-          string currentkey = to_string(ci)+"-"+to_string(amount);
-         if(mp.count(currentkey))  return mp[currentkey];
+         
+         if(mp[ci][amount]!=-1)  return mp[ci][amount];
          
          
           int  notconsider =   totalways(ci+1 ,  amount , coins , mp);
@@ -20,7 +20,7 @@ public:
           int consider = 0 ;
            if( coins[ci]<= amount)
                consider =  totalways(ci , amount-coins[ci] , coins , mp);
-         return mp[currentkey] =  consider +notconsider;
+         return mp[ci][amount] =  consider +notconsider;
          
      }
 };
