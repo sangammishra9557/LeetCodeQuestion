980730047c8b01ffc75a48f0165630bddf224066
Sangam Mishra <sangamhindyana@gmail.com>
2022-07-27 19:41:59 +0530

    Time: 84 ms (5.49%), Space: 13.3 MB (10.98%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -6,6 +6,7 @@ public:
         int m  = text2 .size();
         vector<vector<int>> dp( n+1 , vector<int> ( m+1 , -1));
          return lcs( n-1  ,m-1 , text1 , text2 , dp );
+        //  THIS IS THE CODE FO   TBAULATION 
 //          for(int i = 0 ; i<= n ; i++) dp[i][0] = 0 ; 
 //          for(int i = 0 ; i<= m ; i++) dp[0][i] = 0 ;
 //          for(int i = 1  ; i<= n ; i++)
@@ -20,6 +21,24 @@ public:
 //               }
 //         }
 //          return dp[n][m];
+        // NOW LETS START THE    SPACE OPTIMIZATIOKN SOLUTION 
+        // for(int i = 0 ; i<= n ; i++) dp[i][0] = 0 ;  / DONT NEED THIS LINE   BACESUAE NOW WE DONT HAVE  COLUMNS 
+        
+        vector<int> prev(m+1 , 0) , curr(m+1 , 0);
+         for(int i = 0 ; i<= m ; i++) prev[i] = 0 ;
+         for(int i = 1  ; i<= n ; i++)
+        {
+              for(int j =1 ; j<= m ; j++){
+                  
+                    if( text1[i-1] == text2[j-1])
+                    {  curr[j] =  1 + prev[j-1];
+                    }
+                    else 
+                          curr[j] = max(prev[j] , curr[j-1]);
+              }
+              prev= curr;
+        }
+         return  prev[m];
         
     }
       int lcs( int i , int  j , string &s1 , string &s2 , vector<vector<int>> &dp ){
