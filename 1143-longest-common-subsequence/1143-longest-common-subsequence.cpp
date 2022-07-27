a4f85b0de70542ae4e316a71ff3b9c48d471f5f2
Sangam Mishra <sangamhindyana@gmail.com>
2022-07-27 19:11:51 +0530

    Time: 43 ms (35.92%), Space: 13.2 MB (21.59%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -5,22 +5,39 @@ public:
         int n = text1.size() ; 
         int m  = text2 .size();
         vector<vector<int>> dp( n+1 , vector<int> ( m+1 , -1));
-         for(int i = 0 ; i<= n ; i++) dp[i][0] = 0 ; 
-         for(int i = 0 ; i<= m ; i++) dp[0][i] = 0 ;
-         for(int i = 1  ; i<= n ; i++)
-        {
-              for(int j =1 ; j<= m ; j++){
+         return lcs( n-1  ,m-1 , text1 , text2 , dp );
+//          for(int i = 0 ; i<= n ; i++) dp[i][0] = 0 ; 
+//          for(int i = 0 ; i<= m ; i++) dp[0][i] = 0 ;
+//          for(int i = 1  ; i<= n ; i++)
+//         {
+//               for(int j =1 ; j<= m ; j++){
                   
-                    if( text1[i-1] == text2[j-1])
-                    { dp[i][j] =  1 + dp[i-1][j-1];
-                    }
-                    else 
-                         dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
-              }
-        }
-         return dp[n][m];
+//                     if( text1[i-1] == text2[j-1])
+//                     { dp[i][j] =  1 + dp[i-1][j-1];
+//                     }
+//                     else 
+//                          dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+//               }
+//         }
+//          return dp[n][m];
         
     }
+      int lcs( int i , int  j , string &s1 , string &s2 , vector<vector<int>> &dp ){
+          
+          
+           if(i< 0 || j < 0)  return  0 ; 
+           
+           if(dp[i][j] != -1 )  return dp[i][j];
+          
+          
+           if(s1[i] == s2[j])
+                 return  dp[i][j] = 1+ lcs( i-1 , j-1 , s1 , s2 , dp);
+          
+           else 
+                return dp[i][j] = max(lcs( i , j-1 , s1, s2, dp ), lcs(i-1 , j , s1 , s2 , dp));
+          
+          
+      }
     
     
 };
