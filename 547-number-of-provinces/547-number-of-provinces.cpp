32a69aa64e131ffc9bb8261f217a584e5c0fdef9
Sangam Mishra <sangamhindyana@gmail.com>
2022-08-16 12:28:24 +0530

    Time: 26 ms (87.98%), Space: 13.6 MB (87.69%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -2,30 +2,46 @@ class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
          // this question is vartiation of connected componets in the graph and the vallue present here are  in adjaceny matrix
-         int vertices = isConnected.size();
-          int answer  =  0 ; 
-          vector<int> visited(vertices,0);
-           for(int i = 0 ;i<vertices;i++){
-                if(!visited[i])
-                {  bfs(i , isConnected , visited);
-                  answer++;
-                }
-           }
-         return answer;
+         // int vertices = isConnected.size();
+         //  int answer  =  0 ; 
+         //  vector<int> visited(vertices,0);
+         //   for(int i = 0 ;i<vertices;i++){
+         //        if(!visited[i])
+         //        {  bfs(i , isConnected , visited);
+         //          answer++;
+         //        }
+         //   }
+         // return answer;
+         int n = isConnected.size();
+         int c = 0;
+         vector<int> visited(n , 0);
+         for(int i = 0 ;  i<n ; i++){
+              if(!visited[i])
+              {  c++;
+                  dfs(i ,isConnected ,visited);
+               }
+         }
+         return c ; 
         
         
     }
      private :
         void dfs(int currentcity ,vector<vector<int>> & matrix ,  vector<int>  &visited  ){
-             if(visited[currentcity])
-                 return ; 
-              visited[currentcity] = true ;
+//              if(visited[currentcity])
+//                  return ; 
+//               visited[currentcity] = true ;
              
-             for( int i = 0 ; i<  matrix[currentcity].size(); i++){
-                      if(matrix[currentcity][i] == 1)
-                           dfs(i , matrix , visited);
-                 }
-              
+//              for( int i = 0 ; i<  matrix[currentcity].size(); i++){
+//                       if(matrix[currentcity][i] == 1)
+//                            dfs(i , matrix , visited);
+//                  }
+             if(visited[currentcity]) return ;  
+             visited[currentcity] = true;
+             for(int i =0 ; i <matrix[currentcity].size();i++){
+                  if(matrix[currentcity][i] == 1) // becuase 0 hai iska matlb path hi nhin hai na ;
+                       dfs( i , matrix , visited);
+             }
+             
         }
       void bfs (int src, vector<vector<int>> & matrix  ,  vector<int> & visited)
       {
