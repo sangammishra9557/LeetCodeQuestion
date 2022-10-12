da03977ce85f4ab50da8342167a976fd883157d7
Sangam Mishra <sangamhindyana@gmail.com>
2022-10-12 19:42:49 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -1,40 +1,43 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
-
-
+// } Driver Code Ends
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
-    {         
-         
-         vector<int > costs( V , -1);
-          set<pair<int ,int>>  st ; 
-          int ans = 0 ; 
-          st.insert({0 , 0});
-          while(st.size() > 0){
-                auto val = *st.begin();
-                st.erase(st.begin());
-                int  wt = val.first;
-                int  node = val.second;
-                if(costs[node] != -1)
-                  continue;
-                costs[node] = wt ; 
-                ans += wt;
-                for(auto child : adj[node]){
-                    st.insert({child[1] , child[0]});
-                }
-                 
-          }
-           return ans;
+    {
+    
+     priority_queue<pair<int, int> , 
+    vector<pair<int , int>> , greater<pair<int, int>>> pq ; 
+    
+    
+     vector<int> visited(V , 0);
+     pq.push({ 0 , 0});
+     
+     int sum = 0 ; 
+     while(!pq.empty()){
+           auto it = pq.top();
+           pq.pop();
+           
+           int node = it.second ; 
+           int  wt = it.first;
+           if( visited[node]) continue ; 
+           visited[node] = 1; 
+            sum+= wt ; 
+           
+           for( auto child : adj[node]){
+               
+               if(!visited[child[0]]) pq.push({child[1]  , child[0]});
+           }
+        }
+        return sum ;
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 
 int main()
@@ -65,4 +68,5 @@ int main()
     return 0;
 }
 
-  // } Driver Code Ends
+
+// } Driver Code Ends
