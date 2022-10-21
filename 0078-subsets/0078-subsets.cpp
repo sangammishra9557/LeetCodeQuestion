10046b62eb42fe3b6e82cb4044cf9c88723aca9c
Sangam Mishra <sangamhindyana@gmail.com>
2022-10-21 10:56:12 +0530

    Time: 7 ms (38.23%), Space: 7.1 MB (70.36%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,10 +3,21 @@ public:
     vector<vector<int>> ans ; 
     vector<vector<int>> subsets(vector<int>& nums) {
         
-         vector<int> temp ; 
-         generateSets(nums , 0 , temp);
-          return ans ; 
+         // vector<int> temp ; 
+         // generateSets(nums , 0 , temp);
+         //  return ans ; 
         
+           int n  = nums.size();
+         
+          for( int i = 0 ; i< 1 << n ; i++){
+              
+               vector<int> temp ; 
+               for( int  j  = 0 ; j <n ; j++ ){
+                    if( (i & (1<<j)) != 0) temp.push_back(nums[j]);
+               }
+              ans.push_back(temp);
+          }
+          return ans ; 
     }
         
          
