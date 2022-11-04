1e13de96bcb1ced1edf342e342c1447ee088b1b5
Sangam Mishra <sangamhindyana@gmail.com>
2022-11-04 15:19:38 +0530

    Time: 28 ms (77.11%), Space: 26.5 MB (27.91%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,25 +11,25 @@
  */
 class Solution {
 public:
-    map<int, int> mp ; 
+    //map<int, int> mp ; 
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-         
+         unordered_map<int , int> mp;
          for( int i =0 ; i<inorder.size() ; i++){
            mp[inorder[i]] = i ;    
          }
           int index = 0 ;
-        return build(index , 0 , inorder.size()-1 , inorder , preorder);
+        return build(index , 0 , inorder.size()-1 , inorder , preorder , mp);
      
     }
-    TreeNode * build( int &index ,int instart , int  inend , vector<int> &inorder , vector<int> & preorder){
+    TreeNode * build( int &index ,int instart , int  inend , vector<int> &inorder , vector<int> & preorder , unordered_map<int , int> & mp){
     
          if( index >= preorder.size() || instart> inend) return NULL;
           
          int elemt = preorder[index++];
          TreeNode * root =  new TreeNode(elemt);
          int pos = mp[elemt];
-         root->left  = build(index , instart , pos-1  , inorder , preorder);
-         root->right = build(index  ,pos+1 , inend , inorder  , preorder);
+         root->left  = build(index , instart , pos-1  , inorder , preorder  , mp);
+         root->right = build(index  ,pos+1 , inend , inorder  , preorder , mp);
          
          return root; 
          
