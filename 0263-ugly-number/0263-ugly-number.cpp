de9c79c27ac73f8f54874986678bd02a328aa33c
Sangam Mishra <sangamhindyana@gmail.com>
2022-11-18 22:41:55 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (19.97%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -3,15 +3,14 @@ public:
     bool isUgly(int n) {
  if( n<=0) return false; 
         
-        if(n==1) return true;
-        while(n!=1){
-             if(n%2== 0) n= n/2;
-             else if(n%3==0) n= n/3;
-             else if(n%5==0) n= n/5;
-             else  return false; 
+     
+              
+            while(n%2 == 0) n= n/2;
+            while(n%3 == 0) n = n/3;
+            while(n%5==0) n = n/5;
             
-             
-        }
-        return true;
+            
+        return n==1;       
+
     }
 };
