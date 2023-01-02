c859b84c8f5a2da6e0cff924e378c468de56cff1
Sangam Mishra <sangamhindyana@gmail.com>
2023-01-02 23:39:21 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (33.35%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    
+    
+   void helper(int i , int target ,int sum , int k ,  vector<int> & temp , vector<vector<int>> & ans){
+       
+       if( sum == target && k == 0){
+           ans.push_back(temp);
+           return;
+       }
+       if( k <= 0 || i >9) return ; 
+       
+       helper(i+1 , target ,sum ,  k , temp , ans);
+       
+       temp.push_back(i);
+       helper(i+1 , target , sum + i , k-1 ,temp , ans);
+       temp.pop_back();
+       
+       
+   }
+    
+    
+    
+    
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+       vector<vector<int>> ans; 
+        vector<int> temp ;
+        helper(1 , n , 0 , k , temp , ans );
+        return ans;
+        
+        
+    }
+};
