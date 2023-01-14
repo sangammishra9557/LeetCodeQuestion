eec81b5a1f41cb2cc3e2cce860f15b17fd61aba6
Sangam Mishra <sangamhindyana@gmail.com>
2023-01-14 12:17:15 +0530

    Time: 740 ms (20.96%), Space: 226.6 MB (35.56%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     
-    vector<int> dfs(int v , int parent , vector<int> adj[] , string &labels , vector<int>&ans,vector<int>&visited){
-        visited[v] = 1;
+    vector<int> dfs(int v , int parent , vector<int> adj[] , string &labels , vector<int>&ans){
+
         
         vector<int> temp(26,0);
         for(auto child : adj[v]){
             
-            if(!visited[child]){
-               auto t = dfs( child , v , adj, labels,ans,visited);
+            if(child != parent){
+               auto t = dfs( child , v , adj, labels,ans);
                for(int i = 0; i<26;i++) temp[i]+= t[i];
             }
         }
@@ -23,14 +23,14 @@ public:
         //firstly creating the edge
         vector<int> ans(n,0) ; 
         vector<int> adj[n];
-        vector<int> visited(n,0);
+    
         for( auto edge : edges){
             int a = edge[0] , b = edge[1];
             adj[a].push_back(b);
             adj[b].push_back(a);
 
         }
-      dfs(0 , -1 , adj, labels ,ans,visited);
+      dfs(0 , -1 , adj, labels ,ans);
     return ans;
         
     }
