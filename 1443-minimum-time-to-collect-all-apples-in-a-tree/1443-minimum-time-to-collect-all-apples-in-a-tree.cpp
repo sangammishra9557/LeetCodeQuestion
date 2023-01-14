cce6341271c9fe88d61d29534636be66bf77a55a
Sangam Mishra <sangamhindyana@gmail.com>
2023-01-14 22:57:23 +0530

    Time: 229 ms (23.46%), Space: 64.9 MB (27.18%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     
      int dfs(int v , int parent , vector<int> adj[], vector<bool> &hasApple){
-         
-         
           int totaltime = 0 ,childtime  =0; 
-          
           for( auto child : adj[v]){
                if (child != parent){
                    childtime = dfs(child , v , adj , hasApple);
@@ -21,7 +18,7 @@ public:
         for( auto edge : edges){
             int a = edge[0], b= edge[1];
             adj[a].push_back(b);
-            adj[b].push_back(a);
+           adj[b].push_back(a);
         }
       return  dfs(0 ,-1 ,adj, hasApple);
      // kyunkin question mein agr node jo hain woh tree ki form mein represented hain tph    
