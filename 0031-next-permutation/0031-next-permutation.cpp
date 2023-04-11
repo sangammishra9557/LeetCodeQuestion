d28f6b620779227c1024d875cad9320e1d703344
Sangam Mishra <sangamhindyana@gmail.com>
2023-04-12 01:06:48 +0530

    Time: 8 ms (25.00%), Space: 12.1 MB (41.71%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,7 +3,7 @@ public:
     void nextPermutation(vector<int>& nums) {
          int n = nums.size();
           int idx1 = -1;
-        
+        // yhn ap second last element se search kr kte ho ar milte hi break kr do 
          for(int i = 0 ; i<n-1 ; i++){
               if(nums[i] < nums[i+1]) idx1 = i;
          }
@@ -14,7 +14,7 @@ public:
          }
         
         //now find the idx just greater from break  since after break point all numbers wll be decreasing so search from the back
-        
+         // same below wale llop mein last se search krna start kro ar just greater milter hi break kr do ;
        int idx2 = 0;
         for(int i = idx1+1 ; i<n; i++){
             if(nums[i] > nums[idx1]) idx2 = i;
