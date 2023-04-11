245e1a1f7ce1e04c617bd46d4e2281bc78684c81
Sangam Mishra <sangamhindyana@gmail.com>
2023-04-11 17:50:25 +0530

    Time: 248 ms (22.76%), Space: 134.6 MB (29.96%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,12 +1,46 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> ans(nums.size()) ; 
-         int pos = 0 , neg = 1;
-        for(int i = 0 ; i< nums.size(); i++){
-            if(nums[i] > 0 )
-                ans[pos] = nums[i] , pos+= 2;
-            else ans[neg] = nums[i] , neg+= 2;
+        // vector<int> ans(nums.size()) ; 
+        //  int pos = 0 , neg = 1;
+        // for(int i = 0 ; i< nums.size(); i++){
+        //     if(nums[i] > 0 )
+        //         ans[pos] = nums[i] , pos+= 2;
+        //     else ans[neg] = nums[i] , neg+= 2;
+        // }
+        // return ans;
+        
+        // NOW THE GENERAL Solution that i can code is 
+        vector<int> pos , neg;
+        for(auto it : nums){
+             if( it>0) pos.push_back(it);
+             else neg.push_back(it);
+        }
+        
+        vector<int> ans (nums.size());
+             
+         if(pos.size()>neg.size()){    
+              for(int i = 0 ; i< neg.size();i++){
+                  ans[2*i] = pos[i];
+                  ans[2*i+1] = neg[i];
+              }  
+               int idx = neg.size()*2;
+              for(int i = neg.size() ; i< pos.size();i++){
+                  ans[idx++] = pos[i];
+              }
+         }
+        
+        else {
+               for(int i = 0 ; i<pos.size() ; i++){
+                   ans[2*i] = pos[i];
+                   ans[2*i+1] = neg[i];
+               }
+                 int idx = pos.size()*2;
+            
+               for(int i = pos.size(); i<neg.size();i++){
+                   ans[idx] = neg[i];
+               }
+        
         }
         return ans;
     }
