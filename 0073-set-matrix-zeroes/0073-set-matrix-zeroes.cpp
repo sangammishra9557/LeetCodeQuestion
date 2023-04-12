a0abc38720ba16ff095e0cae0a55b0f54659acb0
Sangam Mishra <sangamhindyana@gmail.com>
2023-04-13 00:47:04 +0530

    Time: 18 ms (40.24%), Space: 13.3 MB (78.30%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,53 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-         int n  = matrix.size();
-         int m = matrix[0].size();
-         vector<int> rows(n ,1);
-         vector<int> cols(m, 1);
-         for(int i = 0 ; i<n ; i++){
-             for(int j = 0 ; j<m ; j++){
-                 if(matrix[i][j] == 0) rows[i] = 0 ,cols[j] = 0;
-             }
-         }
+ // BRUTEFORCE SOLTUION   int n  = matrix.size();
+//          int m = matrix[0].size();
+//          vector<int> rows(n ,1);
+//          vector<int> cols(m, 1);
+//          for(int i = 0 ; i<n ; i++){
+//              for(int j = 0 ; j<m ; j++){
+//                  if(matrix[i][j] == 0) rows[i] = 0 ,cols[j] = 0;
+//              }
+//          }
       
         
-        for(int i= 0 ;i<n; i++){
-            if(rows[i] == 0) {
-                cout<<i;
-                for(int j = 0 ; j<m;j++) matrix[i][j] = 0;
-            }
-        }
-        for(int i = 0 ; i<m ; i++){
-            if(cols[i] == 0){
-                for(int j = 0 ; j<n ; j++){
-                    matrix[j][i] = 0;
-                }
-            }
-        }
+//         for(int i= 0 ;i<n; i++){
+//             if(rows[i] == 0) {
+//                 cout<<i;
+//                 for(int j = 0 ; j<m;j++) matrix[i][j] = 0;
+//             }
+//         }
+//         for(int i = 0 ; i<m ; i++){
+//             if(cols[i] == 0){
+//                 for(int j = 0 ; j<n ; j++){
+//                     matrix[j][i] = 0;
+//                 }
+//             }
+//         }
+    void setZeroes(vector<vector<int>>& matrix) {
+         int n = matrix.size(); 
+         int m = matrix[0].size();
+          
+          int col = 1 ; 
+          for(int i =0 ; i<n; i++){
+              if(matrix[i][0] == 0) col = 0;
+              for(int j = 1 ; j<m ;j++){
+                  if(matrix[i][j] == 0) matrix[i][0] = 0 , matrix[0][j] = 0;
+              }
+          }
+      
+         
+         // start filling the array now ; 
+        
+         for(int i = n-1 ; i>= 0 ; i--){
+             
+             for(int j = m-1 ; j>= 1 ; j--){
+                 
+                  if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] =0;
+             }
+             
+              if(col == 0) matrix[i][0] = 0;
+         }
+    
     }
 };
